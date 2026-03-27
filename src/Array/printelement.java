37235a8b3833b2a13a58cd1ce1fd5f40bc30a8e6
Pallavi Jadhav <pallavijadhav5204@gmail.com>
2026-03-27 12:18:13 +0530

    First Array program added

---
+++ src/Array/printelement.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class printelement {
+
+    public static void main(String[] args)
+    {
+        int arr[] = {20,94,93,48,23};
+
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+}
