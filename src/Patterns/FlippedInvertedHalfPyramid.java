bf8788c8b4d48be41a15ee586d5133b536b3be85
Pallavi Jadhav <pallavijadhav5204@gmail.com>
2026-07-19 20:02:56 +0530

    Patterns Programs added

---
+++ src/Patterns/FlippedInvertedHalfPyramid.java
@@ -0,0 +1,28 @@
+package Patterns;
+
+public class FlippedInvertedHalfPyramid {
+
+    public static void main(String[] args) {
+
+        int n=5;
+
+        //outer loop
+        for(int i=1;i<=n;i++)
+        {
+            // for spaces
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+
+            // for star
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
